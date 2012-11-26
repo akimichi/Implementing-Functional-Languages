75c625d17271ab4d28f63d08f62db165500ba99c
James Cooper <jamescooper@gmail.com>
2012-11-26 01:06:01 -0500

    James C - better indirection

--- src/template/TiState.scala
+++ src/template/TiState.scala
@@ -8,11 +8,12 @@ import utils.Heap.hNull
 
 class TiState(stack : TiStack, dump : TiDump, heap : TiHeap, globals : TiGlobals, stats : TiStats) {
 
-  def eval : List[TiState] =
+  def eval : List[TiState] = {
     if (isFinal)
       List(this)
     else
       this :: this.step.doAdmin.eval
+  }
 
   def doAdmin : TiState = new TiState(stack, dump, heap, globals, stats.incSteps)
 
@@ -29,10 +30,9 @@ class TiState(stack : TiStack, dump : TiDump, heap : TiHeap, globals : TiGlobals
       val argBindings = args.zip(getArgs)
       if (argBindings.length < args.length) throw new Exception("supercombinator " + name + "applied to too few arguments")
       val env = globals ++ argBindings
-      val (newHeap, resultAddr) = instantiate(body, heap, env)
-      val newHeap2 = newHeap.update(stack(args.length))(NInd(resultAddr))
-      val newStack = resultAddr :: stack.drop(args.length + 1)
-      new TiState(newStack, dump, newHeap2, globals, stats)
+      val newHeap = instantiateAndUpdate(body, heap, env, stack(args.length))
+      val newStack = stack.drop(args.length)
+      new TiState(newStack, dump, newHeap, globals, stats)
     }
     case NInd(a) => new TiState(a :: stack.tail, dump, heap, globals, stats)
   }
@@ -98,14 +98,12 @@ class TiState(stack : TiStack, dump : TiDump, heap : TiHeap, globals : TiGlobals
     case EConstr(tag, arity) => throw new Exception("can't update to constructors yet")
     case ELet(false, defs, body) => {
       val (heap1, env1) = defs.foldLeft((heap, env))(instantiateBody)
-      val (heap2, addr) = instantiate(body, heap1, env1)
-      heap2.update(a)(NInd(addr))
+      instantiateAndUpdate(body, heap1, env1, a)
     }
     case ELet(true, defs, body) => {
       val heapEnv = defs.foldLeft((heap, env))(allocateBody)
-      val (heap1, env1) = defs.foldLeft(heapEnv)(instantiateBody)
-      val (heap2, addr) = instantiate(body, heap1, env1)
-      heap2.update(a)(NInd(addr))
+      val (heap1, env1) = defs.foldLeft(heapEnv)(updateBody)
+      instantiateAndUpdate(body, heap1, env1, a)
     }
     case ECase(e, alts) => throw new Exception("can't update to case exprs")
   }
@@ -113,13 +111,13 @@ class TiState(stack : TiStack, dump : TiDump, heap : TiHeap, globals : TiGlobals
   def showState : String = showStack + '\n'
 
   def showStack : String = {
-    def showStackItem(addr : Addr) : String = "   " + addr.showFW + ": " + showStackNode(heap.lookup(addr)) + "\n"
+    def showStackItem(addr : Addr) : String = "   " + addr.showFW + ": " + showStackNode(heap.lookup(addr))
     "Stk [\n" + stack.map(showStackItem).mkString + " ]"
   }
 
   def showStackNode(node : Node) : String = node match {
-    case NAp(fun, arg) => "NAp " + fun.showFW + " " + arg.showFW + " (" + showNode(heap.lookup(arg)) + ")"
-    case node          => showNode(node)
+    case NAp(fun, arg) => "NAp " + fun.showFW + " " + arg.showFW + " (" + showNode(heap.lookup(arg)) + ")\n"
+    case node          => showNode(node) + "\n"
   }
 
   def showNode(node : Node) : String = node match {
@@ -129,6 +127,8 @@ class TiState(stack : TiStack, dump : TiDump, heap : TiHeap, globals : TiGlobals
     case NInd(a)                => "NInd " + a
   }
 
-  def showStats : String = "Total number of steps = " + stats.getSteps + '\n'
+  def showStats : String = "Total number of steps = " + stats.getSteps + '\n' + "Total heap allocation = " + heap.size
 
+  def printHeap : String = heap.addresses.map(a => a + " = " + showStackNode(heap.lookup(a))).mkString
+  
 }
